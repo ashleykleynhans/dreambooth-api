24883d50b87f0a4002047b69d6a97fa153b2c59d
Ashley Kleynhans <ashley.kleynhans@gmail.com>
2023-05-15 18:47:07 +0200

    Start training script

--- train.py
+++ train.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-import json
 import requests
 
 URL = 'http://172.30.61.140:7860'
@@ -17,8 +16,10 @@ def start_training():
     )
 
     print(r.status_code)
-    response_text = r.json()
-    print(json.dumps(response_text, indent=4, default=str))
+
+    if r.status_code == 200:
+        response_text = r.json()
+        print(response_text['Status'])
 
 
 if __name__ == '__main__':
