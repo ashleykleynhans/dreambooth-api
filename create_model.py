03a929a0c63cb2089d20888d6d613ffafb9a2f37
Ashley Kleynhans <ashley.kleynhans@gmail.com>
2023-05-16 06:33:20 +0200

    More paramters

--- create_model.py
+++ create_model.py
@@ -5,14 +5,26 @@ import re
 URL = 'http://172.30.61.140:7860'
 NEW_MODEL_NAME = 'test-model'
 SRC_MODEL = 'v1-5-pruned.safetensors'
+SHARED_DIFFUSERS_SOURCE = ''
+CREATE_FROM_HUB = 'false'
+NEW_MODEL_HUB_URL = ''
+IS_512_RESOLUTION = 'true'
 TRAIN_UNFROZEN = 'false'
+HUGGINGFACE_HUB_TOKEN = ''
+NEW_MODEL_EXTRACT_EMA = 'false'
 
 
 def create_model():
     endpoint = f'{URL}/dreambooth/createModel'
     endpoint += f'?new_model_name={NEW_MODEL_NAME}'
     endpoint += f'&new_model_src={SRC_MODEL}'
+    # endpoint += f'&new_model_shared_src={SHARED_DIFFUSERS_SOURCE}'
+    endpoint += f'&create_from_hub={CREATE_FROM_HUB}'
+    # endpoint += f'&new_model_url={NEW_MODEL_HUB_URL}'
+    endpoint += f'&is_512={IS_512_RESOLUTION}'
     endpoint += f'&train_unfrozen={TRAIN_UNFROZEN}'
+    # endpoint += f'&new_model_token={HUGGINGFACE_HUB_TOKEN}'
+    endpoint += f'&new_model_extract_ema={NEW_MODEL_EXTRACT_EMA}'
 
     r = requests.post(
         endpoint
